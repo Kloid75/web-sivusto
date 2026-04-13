467fe8a071d576f293d0d4581a012a622bf9a46a
Verneri <verneri275@gmail.com>
2026-04-13 14:44:10 +0300

    lisaa funktioita

--- portfolio/summat.js
+++ portfolio/summat.js
@@ -1,17 +1,36 @@
-let a = 3;
-let b = 5;
-let c = 7;
-let d = 15;
-
-function Summa() {
-    alert(a + b)
+var nr1 = document.getElementById('numero1');
+var nr2 = document.getElementById('numero2');
+console.log(nr1)
+console.log(nr2)
+function laskin_summa() {
+    let eka = Number(nr1.value);
+    let toka = Number(nr2.value);
+    Summa(eka, toka)
 }
-function Vähennys() {
-    alert(b - c)
+function laskin_vahennys() {
+    let eka = Number(nr1.value);
+    let toka = Number(nr2.value);
+    Vahennys(eka, toka)
 }
-function Kerto() {
-    alert(c * d)
+function laskin_kerto() {
+    let eka = Number(nr1.value);
+    let toka = Number(nr2.value);
+    Kerto(eka, toka)
 }
-function Jako() {
-    alert(d / a)
+function laskin_jako() {
+    let eka = Number(nr1.value);
+    let toka = Number(nr2.value);
+    Jako(eka, toka)
+}
+function Summa(eka, toka) {
+    alert(eka + toka)
+}
+function Vahennys(eka, toka) {
+    alert(eka - toka)
+}
+function Kerto(eka, toka) {
+    alert(eka * toka)
+}
+function Jako(eka, toka) {
+    alert(eka / toka)
 }
