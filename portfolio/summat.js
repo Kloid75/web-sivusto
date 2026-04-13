075f0cfe2456fa902fd1e209fd44987640a444d8
Verneri <verneri275@gmail.com>
2026-04-13 12:19:01 +0300

    Summat

---
+++ portfolio/summat.js
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 5;
+let c = 7;
+let d = 15;
+
+function Summa() {
+    alert(a + b)
+}
+function Vähennys() {
+    alert(b - c)
+}
+function Kerto() {
+    alert(c * d)
+}
+function Jako() {
+    alert(d / a)
+}
