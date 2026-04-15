db8420ea3fd80d2eeae692928034a7735bb4088f
Verneri <verneri275@gmail.com>
2026-04-15 13:02:16 +0300

    onclick

--- portfolio/keskustelusivu.js
+++ portfolio/keskustelusivu.js
@@ -1,1 +1,14 @@
 console.log("keskustelusivu avattu")
+function send(e) {
+    e.preventDefault();
+    let birthdayField = e.target.form.syntymäpäivä.value;
+    console.log(birthdayField);
+    let emailField = e.target.form.sähköposti.value;
+    console.log(emailField);
+    let typeField = e.target.form.valinta.value;
+    console.log(typeField);
+    let usageField = e.target.form.check.value;
+    console.log(usageField);
+    let bodyField = e.target.form.kommentti.value;
+    console.log(bodyField);
+}
