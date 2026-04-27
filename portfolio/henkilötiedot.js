34ec32a68b343e8d84a4993c90ee432cc5169218
Verneri <verneri275@gmail.com>
2026-04-27 12:23:17 +0300

    if ja else

--- portfolio/henkilötiedot.js
+++ portfolio/henkilötiedot.js
@@ -30,18 +30,43 @@ let henkilot = [
         driversLicense: true
     },
 ]
+function lisaatiedot() {
+    var n = document.getElementById("name").value
+    var a = document.getElementById("age").value
+    var j = document.getElementById("job").value
+    var d = document.getElementById("license").checked
+    if (a <= 0) {
+        alert("Iän pitää olla positiivinen luku")
+    }
+    else {
+    var henk = {
+        name: n,
+        age: a,
+        job: j,
+        driversLicense: d,
+    }
+    henkilot.push(henk)
+    luoRivit()
+    }
+}
 var rivit = document.getElementById("rivit")
 function luoRivit() {
-    rivit.value = ""
+    rivit.innerHTML = ""
     for (var henkilo of henkilot) {
         var rivi = document.createElement('tr')
         var nimi = document.createElement('td')
         nimi.innerHTML = henkilo.name
         rivi.append(nimi)
         var ika = document.createElement('td')
+        if (henkilo.age >= 18) {
+            henkilo.age = henkilo.age + "🍺"
+        }
         ika.innerHTML = henkilo.age
         rivi.append(ika)
         var tyo = document.createElement('td')
+        if (henkilo.job == "Opiskelija") {
+            henkilo.job = henkilo.job + "🎓️"
+        }
         tyo.innerHTML = henkilo.job
         rivi.append(tyo)
         var ajokortti = document.createElement('td')
